57f1a9c5a3ab5d3cc2c73d1da93bfb58de786b2d
Yuşa Sarısoy <yusasarisoy@icloud.com>
2022-11-13 13:07:36 +0300

    Add a failing test for TDD

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -26,4 +26,24 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import XCTest
+@testable import Numero
 
+final class ConverterTests: XCTestCase {
+  var converter: Converter!
+
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+  }
+}
